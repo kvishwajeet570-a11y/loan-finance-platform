9d466a4f22831881d1ce7783f5f59e9c0ccad8c4
Vishwajeet <kvishwajeet570@gmail.com>
2026-06-23 04:07:45 +0530

    Use SSL SMTP 465

--- backend/src/utils/sendEmail.ts
+++ backend/src/utils/sendEmail.ts
@@ -6,35 +6,19 @@ export const sendEmail = async (
   text: string
 ) => {
   try {
-    console.log("EMAIL_USER =", process.env.EMAIL_USER);
-    console.log("EMAIL_PASS EXISTS =", !!process.env.EMAIL_PASS);
-
     const transporter = nodemailer.createTransport({
       host: "smtp-relay.brevo.com",
-      port: 2525,
-      secure: false,
+      port: 465,
+      secure: true,
 
       auth: {
-        user: process.env.EMAIL_USER!,
-        pass: process.env.EMAIL_PASS!,
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
       },
-
-      connectionTimeout: 15000,
-      greetingTimeout: 15000,
-      socketTimeout: 15000,
     });
 
-    console.log("VERIFYING SMTP...");
-
-    await transporter.verify();
-
-    console.log("SMTP VERIFIED");
-
     const info = await transporter.sendMail({
-      from: {
-        name: "Loan Finance Platform",
-        address: process.env.EMAIL_USER!,
-      },
+      from: `"Loan Finance Platform" <${process.env.EMAIL_USER}>`,
       to,
       subject,
       text,
